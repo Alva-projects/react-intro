803fab136f2130dc724a5eaad37ff452c9b595a3
Alva Lundberg <alva.lundberg@hotmail.se>
2025-11-21 09:26:46 +0100

    Link tags closed
    Navbar works

--- Portfolio/src/Navbar.jsx
+++ Portfolio/src/Navbar.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom"
 function Navbar () {
     return(
         <>
-        <div className="navbar-container">
-            <nav className="navbar-items">
-                <ul>
-                    <li><Link to="/"/>Home</li>
-                    <li><Link to="portfolio"/>Project</li>
-                    <li><Link to="contact"/>Contact</li>
-                </ul>
-            </nav>
-        </div>
+            <div className="navbar-container">
+                <nav className="navbar-items">
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="portfolio">Project</Link></li>
+                        <li><Link to="contact"> Contact</Link></li>
+                    </ul>
+                </nav>
+            </div>
         </>
     )
 }
